ce3af826eb5c7d150d27dabb81693a14c8ed15c5
notdryft <gui.corre@gmail.com>
2015-01-04 15:01:46 +0100

    Add wifi modprobe.d task

--- roles/wifi/templates/wifi_check.sh
+++ roles/wifi/templates/wifi_check.sh
@@ -9,8 +9,8 @@ ping -q -c1 $keepalive_host >> /dev/null
 if [ "$?" -ne "0" ]; then
   echo "`date` WIFI DOWN" >> /var/log/wifi.log
   ifdown wlan0
-  rmmod 8192cu
-  modprobe 8192cu
+  rmmod {{ wifi_mod }}
+  modprobe {{ wifi_mod }}
   ifup wlan0
   echo "`date` WIFI UP" >> /var/log/wifi.log
 fi
